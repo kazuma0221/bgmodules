9887998732b2962252a5f827a4f490c3c601beb8
kazuma0221 <siamez@hotmail.com>
2026-02-12 06:17:07 +0900

    dataclasses.dataclassを使う形に変更 テストコードを整理

--- boardgame/table.py
+++ boardgame/table.py
@@ -1,24 +1,26 @@
+from dataclasses import dataclass, field
 from boardgame.rules import Rules
 from boardgame.player import Player
 
-class Table():
+@dataclass
+class Table:
     '''ゲーム卓。ゲームに必要なデータを保持する。
     :param Rules rules: ゲームのルール。
     :param list[Players] players: ゲームのプレイヤーのlist。
-    :param list pieces: ゲームの内容物のlist。型は何でもよく、Pieceクラスに限らない。'''
-    def __init__(self, rules:Rules, players:list[Player], pieces:list, inputData:dict={}):
-        self.rules: Rules = rules
-        self.players: list[Player] = players
-        self.pieces: list = pieces
-        self.inputData: dict = inputData
-        self.event: dict = {}
+    :param list pieces: ゲームの内容物のlist。型は何でもよく、Pieceクラスに限らない。
+    :param dict inputData: PRからAPに入力するデータの辞書。作成時は空でもよい。
+    :param dict event: APからPRに出力するデータの辞書。作成時は空にすることを推奨する。'''
+    rules: Rules
+    players: list[Player]
+    pieces: list
+    inputData: dict = field(default_factory=dict)
+    event: dict = field(default_factory=dict)
 
 # テスト
 if __name__ == '__main__':
     # プレイヤーを作成
-    from boardgame.player import makePlayers
-    from boardgame.player import PlayerType as Type
-    players = makePlayers(types=[Type.HUMAN, Type.AI_RANDOM], names=['You', 'CPU'])
+    from boardgame.player import makePlayers, PlayerType as PT
+    players = makePlayers(types=[PT.HUMAN, PT.AI_RANDOM], names=['You', 'CPU'])
 
     # ルールを適当に作成
     class TestRules(Rules):
@@ -28,10 +30,11 @@ if __name__ == '__main__':
     # コマを適当に作成
     from bgpieces.color import Color
     from bgpieces.piece import Piece
-    pieces = []
-    for color in Color.items():
-        pieces.append(Piece(color=color))
+    pieces = [Piece(color=color) for color in Color.items()]
 
+    # 本題：テーブルを作り、内容を確認
+    print('---------')
     table = Table(rules=TestRules(), players=players, pieces=pieces)
     for elem in table.__dict__.items():
         print(elem)
+    print('---------')
