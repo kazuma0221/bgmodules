fbfb9e5e56d16fc5ab568e96efe38fa98827c64c
Kazuma Suzuki <tartegamestokyo@gmail.com>
2026-06-30 14:18:11 +0900

    proc.do()が未実装のときはNotImplementedErrorを返すよう修正

--- boardgame/proc.py
+++ boardgame/proc.py
@@ -4,6 +4,8 @@ from table import Table
 class Proc(ABC):
     '''ボードゲームの手順を定義する親クラス。このクラスをStateパターンで継承して各処理を記述する。'''
     @abstractmethod
-    def do(self, table:Table)->dict:
-        '''処理の実装。出力用の辞書を必ず返す。'''
-        pass
+    def do(self, table:Table) -> dict:
+        '''処理の実装。出力用の辞書を必ず返す。
+        Args:
+            table (Table): データを保持するゲーム卓。'''
+        raise NotImplementedError
