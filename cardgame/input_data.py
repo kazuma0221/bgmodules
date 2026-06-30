7b35efb34d259812898a8d2533e5ca5cfecddae3
Kazuma Suzuki <tartegamestokyo@gmail.com>
2026-06-30 19:14:54 +0900

    InputDataをクラス化

--- cardgame/input_data.py
+++ cardgame/input_data.py
@@ -1,31 +1,34 @@
 from cardgame.player_type import PlayerType as T
 
-inputData = {
-    'player_names': [],
-    'player_types': [],
-    'choice': -1,
-    'clear': False
-}
+class InputData:
+    def __init__(self, names:list[str]=[], types:list[T]=[], clear:bool=False):
+        self.dic = {
+            'player_names': names,
+            'player_types': types,
+            'choice': -1,
+            'clear': clear,
+        }
 
-def setNames(names):
-    inputData['player_names'] = names
+    def setNames(self, names):
+        self.dic['player_names'] = names
 
-def setTypes(types):
-    inputData['player_types'] = types
+    def setTypes(self, types):
+        self.dic['player_types'] = types
 
-def setChoice(choice):
-    inputData['choice'] = choice
+    def setChoice(self, choice):
+        self.dic['choice'] = choice
 
-def setClear(clear):
-    inputData['clear'] = clear
+    def setClear(self, clear):
+        self.dic['clear'] = clear
 
-def set(names:list[str], types:list[T]):
-    inputData['player_names'] = names
-    inputData['player_types'] = types
+    def set(self, names:list[str], types:list[T]):
+        self.dic['player_names'] = names
+        self.dic['player_types'] = types
 
-def makeTestData(number=4):
-    setNames(['You', 'Abigail', 'Benjamin', 'Camille', 'Dennis'])
-    setTypes([T.HUMAN, T.AI_RANDOM, T.AI_RANDOM, T.AI_RANDOM, T.AI_RANDOM])
-    del inputData['player_names'][number:5]
-    del inputData['player_types'][number:5]
-    return inputData
+def makeTestData(self, number=4):
+    names = ['You', 'Abigail', 'Benjamin', 'Camille', 'Dennis']
+    types = [T.HUMAN, T.AI_RANDOM, T.AI_RANDOM, T.AI_RANDOM, T.AI_RANDOM]
+    test_data = InputData(names, types)
+    del test_data['player_names'][number:5]
+    del test_data['player_types'][number:5]
+    return test_data
