9e834782ed0c1a817cb0bd2c6477b39fe0d5c79a
Kazuma Suzuki <tartegamestokyo@gmail.com>
2026-07-02 12:07:12 +0900

    InputDataを加工可能(frozen=False)に変更

--- boardgame/dto.py
+++ boardgame/dto.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
 from boardgame.event_type import EventType
 
-@dataclass(frozen=True)
+@dataclass(frozen=False)
 class InputData:
     '''PR層からAP層へ、ゲーム入力を引き渡すためのデータ転送オブジェクト(DTO)の基底クラス。
+    PR層からの入力データをAP層が消費・加工しながらゲームを進行するので、frozen=False（プロシージャ内部での書き換えやクリアが可能）にしている。
     継承時の引数順序エラー（TypeError）を防ぐため、デフォルト値のあるフィールドは定義しない。
     '''
     pass
